8d508ba060cf0d4ad458046be6afc73eff13157a
Joost de Moel <joost@ngti.nl>
2015-07-21 15:22:38 +0200

    Squashed 'SampleLib/' content from commit b7c4db3
    git-subtree-dir: SampleLib
    git-subtree-split: b7c4db3876cff4bc8b7e9adef922643cd7100ee7

---
+++ SampleLib/Classes/Mammie.h
@@ -0,0 +1,19 @@
+//
+//  Mammie.h
+//  Pods
+//
+//  Created by Joost de Moel on 16/07/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mammie : NSObject
+
+- (void)kots;
+- (NSString*)geefEenKoekje;
+
+- (void)test;
+
+
+@end
